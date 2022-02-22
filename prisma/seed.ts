720a3dff891fbb049c00cabbf7676873f6d83b94
Jiyeon <a1919361@gmail.com>
2022-02-22 13:43:27 +0900

    add seed

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient} from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main(){
+  await prisma.user.create({
+    data: {
+      email: 'test@gmail.com',
+    }
+  });
+
+  await prisma.post.create({
+    data: {
+      title: 'test post',
+    }
+  });
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+}).finally(async () => {
+  await prisma.$disconnect();
+})
